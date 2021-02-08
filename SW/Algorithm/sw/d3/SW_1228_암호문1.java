506c52c861d02eb133d13da02ef59bef551a192a
bjgu97 <bjgu97@naver.com>
2021-02-08 17:21:04 +0900

    0208 17:20

--- SW/Algorithm/sw/d3/SW_1228_암호문1.java
+++ SW/Algorithm/sw/d3/SW_1228_암호문1.java
@@ -6,18 +6,26 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
-
+//0.12320
 public class SW_1228_암호문1 {
+	static BufferedReader br;
+	static StringTokenizer st;
+	static StringBuilder sb;
+	
+	static List<Integer> list;
+	
+	static int cnt;
+	
 	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		StringBuilder sb = new StringBuilder();
+		br = new BufferedReader(new InputStreamReader(System.in));
+		sb = new StringBuilder();
+		
 		for(int t = 0; t < 10; t++) {
 			// 암호문 길이 N 입력받기
 			int N = Integer.parseInt(br.readLine());
 			
 			// 암호문 N개 입력받기
-			List<Integer> list = new ArrayList<Integer>();
+			list = new ArrayList<Integer>();
 			st = new StringTokenizer(br.readLine());
 			
 			for(int n = 0; n < N; n++) {
@@ -29,7 +37,7 @@ public class SW_1228_암호문1 {
 			
 			//삽입 명령
 			st = new StringTokenizer(br.readLine());
-			int cnt = 0;
+			cnt = 0;
 			while(true) {
 				if(st.nextToken().charAt(0) == 'I') {
 					cnt++;
@@ -43,13 +51,12 @@ public class SW_1228_암호문1 {
 				}
 			
 			sb.append("#").append(t+1).append(" ");
-			for(int i = 0; i < 10; i++) {
+			for(int i = 0; i < 10; ++i) {
 				sb.append(list.get(i)).append(" ");	
 			}
 			sb.append("\n");
 		}
 		System.out.println(sb);
-
 		}
 	}
 
