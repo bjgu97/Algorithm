36a57dd63ca02607a22372d7018abca99b2189a8
bjgu97 <37968260+bjgu97@users.noreply.github.com>
2021-03-13 20:01:20 +0900

    Delete etc directory
    delete

--- etc/practice.java
+++
@@ -1,8 +0,0 @@
-
-public class practice {
-public static void main(String[] args) {
-	String num = Character.toString('*');
-	System.out.println(num);
-	System.out.println(num.get == Character.toString('*'));
-}
-}
